e9abeea17d2238ee1dad346a70c4263aed1bff0b
Rei Setogawa <rset6207@uni.sydney.edu.au>
2022-06-14 00:22:04 +1000

    debugging css

---
+++ dist/index.5217163c.js
@@ -0,0 +1,30 @@
+function task() {
+    this.taskname = "";
+    this.taskdate = "";
+    this.duration = 0;
+    this.priority = "";
+}
+console.log("hello");
+function timeline() {
+    var today = new Date();
+    var date = today.getDate();
+    if (date > 1 && date <= 21 && (today.getMonth() == 3 || today.getMonth() == 5 || today.getMonth() == 8 || today.getMonth() == 10)) {
+        var text = document.createTextNode("");
+        for(var i = date - 1; i <= date + 9; i++){
+            var span = document.createElement("span");
+            if (i == date) text = document.createTextNode("today");
+            else text = document.createTextNode("" + i);
+            span.appendChild(text);
+            document.getElementById("timeline").appendChild(span);
+        }
+    }
+}
+function newtask() {
+    var newtask1 = new task();
+    newtask1.taskname = document.getElementById("taskname").value;
+    newtask1.taskdate = document.getElementById("date").value;
+    newtask1.duration = parseInt(document.getElementById("duration").value);
+    newtask1.priority = document.getElementById("priority").value;
+}
+
+//# sourceMappingURL=index.5217163c.js.map
